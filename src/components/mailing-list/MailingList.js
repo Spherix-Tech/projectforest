e6a46331a996811db177af781b287b1206909d98
Afra Dawood <3frahmalik@gmail.com>
2022-07-07 16:08:42 +0400

    Newsletter section responsive

--- src/components/mailing-list/MailingList.js
+++ src/components/mailing-list/MailingList.js
@@ -3,14 +3,14 @@ import ImageComponent from "../shared/ImageComponent";
 
 const MailingList = () => {
   return (
-    <div className="w-full text-primary flex flex-row justify-between py-8 bg-mailBgColor lg:px-60 px-6">
-      <ImageComponent src={"/assets/mail-list/mail-list-logo.svg"} alt="Project forest logo" />
-      <div className="flex flex-col w-3/4">
-        <h2 className=" font-semibold text-[34px] py-2 text-textTitle">
+    <div className="w-full text-primary flex lg:flex-row flex-col justify-between py-8 bg-mailBgColor lg:px-60 px-6">
+      <ImageComponent src={"/assets/mail-list/mail-list-logo.svg"} alt="Project forest logo" className="hidden lg:flex justify-center items-center lg:h-42 mr-6" />
+      <div className="flex flex-col w-full">
+        <h3 className=" text-[25px] font-semibold py-2 text-textTitle">
           Take your First Step to a Greener Tomorrow
-        </h2>
+        </h3>
         <p className="text-[#1D3C34] text-sm pb-6">Join our Mailing List</p>
-        <div className="flex flex-row gap-4 space-between">
+        <div className="flex lg:flex-row flex-col gap-4 space-between">
           <input
             type="text"
             placeholder="First Name"
@@ -22,8 +22,8 @@ const MailingList = () => {
             className="py-3 px-4 w-full placeholder:text-sm outline-none rounded-md"
           ></input>
         </div>
-        <div className="flex flex-row items-center justify-between py-4">
-          <p className="w-3/4">
+        <div className="flex flex-row items-center justify-between pt-6">
+          <p className="w-3/4 text-sm lg:text-[16px]">
             Get our emails to stay in the know on what’s happening in the field
             and ways you can get involved.
           </p>
