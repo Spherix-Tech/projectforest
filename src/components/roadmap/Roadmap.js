1a5f4b11ecd52b46c3109e47f3e96a2b73a9b6d2
Afra Dawood <3frahmalik@gmail.com>
2022-07-13 09:43:43 +0400

    Roadmap desktop version added

--- src/components/roadmap/Roadmap.js
+++ src/components/roadmap/Roadmap.js
@@ -6,19 +6,18 @@ const DataArr = getDataBySectionName("roadmap");
 
 const Roadmap = () => {
   return (
-    <div className=" text-textDescription lg:px-60 px-6 py-20" id="roadmap">
-      <h2 className=" text-textTitle text-[40px] font-bold">Road<span className="font-normal">map</span></h2>
+    <div className=" text-textDescription py-10 bg-[#F2F2F2]" id="roadmap">
+      <h3 className=" text-textTitle text-[40px] font-bold lg:px-60 px-6">Roadmap</h3>
       <div className="w-full relative flex justify-center">
-      <ImageComponent src={"/assets/roadmap/roadmap.png"} className="mt-16"/>
-      <h2 className="text-primary text-[50px] font-bold absolute bottom-12 ">2022-<span className="font-normal">2023</span></h2>
+      <ImageComponent src={"/assets/roadmap/roadmap-1.png"} className="mt-16 mb-5"/>
     
-      {DataArr.map((e, i) => {
+      {/* {DataArr.map((e, i) => {
           return (
             <div key={i} className={`${e.textPosition} absolute w-[180px] `} >
             <p className='text-[12px] text-textDescription text-opacity-70'>{e.text}</p>
             </div>
           );
-        })}
+        })} */}
         </div>
     
     </div>
