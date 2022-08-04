9ed763323a3c41983568f7fcf9a2d2dfc9038881
Muzummil Iqbal <muzummil.iqbal@gmail.com>
2022-08-04 12:27:08 +0400

    Pages created

--- src/services/data-files/navbarData.js
+++ src/services/data-files/navbarData.js
@@ -7,28 +7,34 @@ const allData = [
   {
     name: "Gameplay",
     link: "/gameplay",
-    icon_src: "/assets/navbar/home.svg",
-  },
-  {
-    name: "HOW IT WORKS",
-    link: "#how-it-works",
     icon_src: "/assets/navbar/how-it-works.svg",
   },
+
   {
     name: "NFTs",
     link: "/nfts",
     icon_src: "/assets/navbar/nft.svg",
   },
   {
-    name: "TOKEN",
-    link: "#token",
+    name: "FAQ",
+    link: "/faq",
     icon_src: "/assets/navbar/token.svg",
   },
   {
-    name: "ROADMAP",
-    link: "#roadmap",
+    name: "About Us",
+    link: "/about-us",
     icon_src: "/assets/navbar/roadmap.svg",
   },
+  // {
+  //   name: "ROADMAP",
+  //   link: "#roadmap",
+  //   icon_src: "/assets/navbar/roadmap.svg",
+  // },
+  // {
+  //   name: "HOW IT WORKS",
+  //   link: "#how-it-works",
+  //   icon_src: "/assets/navbar/how-it-works.svg",
+  // },
 ];
 
 export const getAllNavbarData = () => allData;
