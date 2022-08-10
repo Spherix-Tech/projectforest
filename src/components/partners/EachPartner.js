0aee726766bb964aa81ec99602570559b1c6b274
Muzummil Iqbal <muzummil.iqbal@gmail.com>
2022-08-10 14:04:21 +0400

    Partners height

--- src/components/partners/EachPartner.js
+++ src/components/partners/EachPartner.js
@@ -5,7 +5,11 @@ function EachPartner({ imageName }) {
   return (
     <ImageComponent
       src={"/assets/partners/" + imageName}
-      className="w-[12rem] h-[4rem] md:w-[18rem] md:h-[5.5rem]"
+      className={
+        imageName == "future-trees.png"
+          ? " w-[11rem] h-[3.5rem] md:h-[4.5rem] md:w-[13rem]"
+          : " w-[14rem] h-[3.2rem] md:w-[18rem]"
+      }
     />
   );
 }
