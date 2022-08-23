6063e43430b4af8d561aa617e8889167bc37fbbc
Afra Dawood <3frahmalik@gmail.com>
2022-08-23 15:47:27 +0400

    FAQ multiple open asnwers issue fixed

--- src/components/faq/FAQAccordion.js
+++ src/components/faq/FAQAccordion.js
@@ -1,42 +1,41 @@
-import React, { useState } from "react";
-import ImageComponent from "../shared/ImageComponent";
+import React, { useState } from 'react';
+import ImageComponent from '../shared/ImageComponent';
 
-const FAQAccordion = ({ question, answer, id }) => {
-  const [isActive, setIsActive] = useState(false);
-
-  // const toggle = id => {
-  //   if(isActive === id) {
-  //     return setIsActive(null);
-  //   }
-  //   setIsActive(id)
-  // }
+const FAQAccordion = ({
+  question,
+  answer,
+  id,
+  selectedItem,
+  setSelectedItem,
+}) => {
+  const isActive = id === selectedItem;
 
   return (
-    
-    <div className="pt-2 w-full border-b-[1px] border-dashed border-b-[#666] border-opacity-50">
+    <div className='pt-2 w-full border-b-[1px] border-dashed border-b-[#666] border-opacity-50'>
       <div
         className={
-          "flex flex-row w-full cursor-pointer justify-between bg-opacity-40 p-4 pl-0"
+          'flex flex-row w-full cursor-pointer justify-between bg-opacity-40 p-4 pl-0'
         }
-        onClick={() => setIsActive(!isActive) }
+        onClick={() => setSelectedItem(id)}
       >
-        <div className="flex flex-row gap-3 items-center">
+        <div className='flex flex-row gap-3 items-center'>
           <ImageComponent
-            src="/assets/dot-icon.png"
-            alt="Project Forest"
-            className="h-2"
+            src='/assets/dot-icon.png'
+            alt='Project Forest'
+            className='h-2'
           />
-         
-          <h2 className=" text-[15px] lg:text-[18px] text-textTitle font-medium">
-          {question}
+
+          <h2 className=' text-[15px] lg:text-[18px] text-textTitle font-medium'>
+            {question}
           </h2>
-        
         </div>
-        <p className=" text-textTitle text-[25px]">{isActive ? "-" : "+"}</p>
+        <p className=' text-textTitle text-[25px]'>{isActive ? '-' : '+'}</p>
       </div>
-      {isActive && ( 
-        <div className=" flex flex-row justify-between py-4">
-          <div className=" pl-4 pr-8 lg:text-sm text-xs text-textTitle">{answer}</div>
+      {isActive && (
+        <div className=' flex flex-row justify-between py-4'>
+          <div className=' pl-4 pr-8 lg:text-sm text-xs text-textTitle'>
+            {answer}
+          </div>
         </div>
       )}
     </div>
@@ -44,4 +43,3 @@ const FAQAccordion = ({ question, answer, id }) => {
 };
 
 export default FAQAccordion;
-
