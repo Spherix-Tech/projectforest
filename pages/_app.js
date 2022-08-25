56bd0b307a950df0f8eba6a46557ea23a29e8efb
Muzummil Iqbal <muzummil.iqbal@gmail.com>
2022-08-25 09:08:14 +0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import ReactGA from "react-ga";
 ReactGA.initialize("G-XJKS1PTP6Y");
 if (typeof window !== "undefined") {
+  console.log(window.location.pathname + window.location.search);
   ReactGA.pageview(window.location.pathname + window.location.search);
 }
 function MyApp({ Component, pageProps }) {
