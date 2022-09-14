f41486e73fd05ee852ddc7e399a9963572452ffb
Afra Dawood <3frahmalik@gmail.com>
2022-09-14 13:55:52 +0400

    Beta page + hero component update

--- src/components/beta/BetaComponent.js
+++ src/components/beta/BetaComponent.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Hero from "./hero/Hero";
 
 function BetaComponent() {
-  return <div>BetaComponent</div>;
+  return (
+  <div>
+    <Hero />
+  </div>
+  );
 }
 
 export default BetaComponent;
---
+++ src/components/beta/hero/Hero.js
@@ -0,0 +1,39 @@
+import React from "react";
+import ImageComponent from "../../shared/ImageComponent";
+import { DISCORD_LINK } from "../../../utilities/constants";
+
+const Hero = () => {
+  return (
+    <div className="flex flex-col gap-7 items-center text-center section-spacing">
+      <h4 className=" text-6xl text-[#3A3A3C] font-extrabold">
+        Your Journey Starts Here!
+      </h4>
+
+      <p className=" text-primaryBlue text-xl font-semibold  ">
+        Project Forest’s closed beta will be launching shortly.<br></br>
+        Be the first one to experience Grow-to-Earn and earn free NFTs.
+      </p>
+      <div className="flex flex-row pt-1 gap-4">
+        <button className="btnPrimary cursor-pointer lg:w-56 w-40 mr-0">
+          <ImageComponent
+            src={"/assets/wallet/metamask.svg"}
+            className="mr-[12px]"
+          />
+          <p>Connect</p>
+        </button>
+        <a
+          href={DISCORD_LINK}
+          target="_blank"
+          className="cursor-pointer"
+          rel="noreferrer"
+        >
+          <button className="cursor-pointer btnSecondary  lg:w-56 w-40 mr-0">
+            Discord
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
