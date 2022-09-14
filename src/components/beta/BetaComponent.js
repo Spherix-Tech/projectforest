daf8b89dd5076a68a5dd973ac12d2c009c206fe6
Afra Dawood <3frahmalik@gmail.com>
2022-09-14 15:55:46 +0400

    Timeline section images added

--- src/components/beta/BetaComponent.js
+++ src/components/beta/BetaComponent.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Hero from "./hero/Hero";
 import Benefits from "./benefits/benefits";
+import Timeline from "./timeline/Timeline";
 
 function BetaComponent() {
   return (
-  <div>
-    <Hero />
-    <Benefits />
-  </div>
+    <div>
+      <Hero />
+      <Benefits />
+      <Timeline />
+    </div>
   );
 }
 
---
+++ src/components/beta/timeline/Timeline.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Timeline = () => {
+  return (
+    <div className='py-10'>
+<h2>Project Forest Launch Timeline</h2>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem 
+Ipsum has been the industry’s standard dummy</p>
+    </div>
+  )
+}
+
+export default Timeline
