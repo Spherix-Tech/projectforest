d6f5c5a379eb3413a78198ab3455b254f7bbef9b
Afra Dawood <3frahmalik@gmail.com>
2022-09-15 16:07:00 +0400

    Litepaper linked to Navbar

--- pages/index.js
+++ pages/index.js
@@ -148,7 +148,6 @@ export default function Home() {
             {/* NFT Slider Section */}
             <div className="nft-impact-section-bg">
               <NFTSlider DataArr={DataArr} />
-
               {/* Token Section */}
               <Token />
             </div>
@@ -156,10 +155,11 @@ export default function Home() {
             {/* Roadmap Section */}
             <Roadmap />
 
+            {/* Partners Section */}
+            <Partners />
+
             {/* Reforestation Section */}
             <Reforestation />
-
-            <Partners />
 
             {/* Newsletter Section */}
             {/* <MailingList /> */}
