97e99ab733b00fba14a9cd44ec2b090d5fb555aa
Afra Dawood <3frahmalik@gmail.com>
2022-09-19 18:06:24 +0400

    Claim beta section update

--- src/components/beta/BetaComponent.js
+++ src/components/beta/BetaComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Hero from './hero/Hero';
 import Benefits from './benefits/benefits';
-import Timeline from './timeline/Timeline';
+import ClaimBeta from './claim-beta/ClaimBeta';
 import ImageComponent from '../shared/ImageComponent';
 
 function BetaComponent() {
@@ -10,16 +10,21 @@ function BetaComponent() {
       <Hero />
       <div className='bg-[#f3f1cb] w-full section-spacing'>
         <Benefits />
-        {/* <Timeline /> */}
+        <ClaimBeta />
       </div>
       <ImageComponent
-        src={'/assets/beta/timeline/left-tree.png'}
-        className='absolute left-0 bottom-16 h-[400px] z-50 lg:block hidden'
+        src={'/assets/beta/claim/left-tree.png'}
+        className='absolute left-0 bottom-[700px] h-[400px] z-50 lg:block hidden'
       />
       <ImageComponent
-        src={'/assets/beta/timeline/right-tree.png'}
-        className='absolute right-0 bottom-16 h-[400px] z-50 hidden lg:block'
+        src={'/assets/beta/claim/right-tree.png'}
+        className='absolute right-0 bottom-[700px] h-[400px] z-50 hidden lg:block'
       />
+       {/* <ImageComponent
+        src={'/assets/beta/claim/claim-bg.png'}
+        className='absolute left-9 bottom-[350px] h-[800px] z-50 hidden lg:block w-full'
+      /> */}
+
     </div>
   );
 }
---
+++ src/components/shared/ClaimBetaCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ImageComponent from "./ImageComponent";
+
+function ClaimBetaCard({ title, text, social, href }) {
+  return (
+    <div className=" flex items-center bg-black font-semibold justify-between rounded-2xl md:py-5 py-4 border-2 border-white ">
+        <div className="flex flex-row items-center">
+      <ImageComponent src="assets/beta/claim/quest-icon.svg" className="h-12 md:px-6 px-4" />
+      <div className="flex flex-col items-start">
+        <h2 className="text-[#CAFF04] md:text-xl text-[15px] pb-2">{title}</h2>
+        <p className="text-left text-[12px] md:text-base">{text}</p>
+      </div>
+      </div>
+      <a href={href} target="_blank" rel="noreferrer">
+      <ImageComponent
+        src={"assets/beta/claim/" + social}
+        alt="icon"
+        className="md:h-12 h-10 md:px-6 px-4"
+      />
+      </a>
+    </div>
+  );
+}
+
+export default ClaimBetaCard;
