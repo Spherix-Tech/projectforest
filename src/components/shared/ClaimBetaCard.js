115d13b79ba565d9f9d6b23cdda29a4a797722e5
Afra Dawood <3frahmalik@gmail.com>
2022-09-19 18:19:27 +0400

    Text color added

--- src/components/shared/ClaimBetaCard.js
+++ src/components/shared/ClaimBetaCard.js
@@ -8,7 +8,7 @@ function ClaimBetaCard({ title, text, social, href }) {
       <ImageComponent src="assets/beta/claim/quest-icon.svg" className="h-12 md:px-6 px-4" />
       <div className="flex flex-col items-start">
         <h2 className="text-[#CAFF04] md:text-xl text-[15px] pb-2">{title}</h2>
-        <p className="text-left text-[12px] md:text-base">{text}</p>
+        <p className="text-left text-[12px] md:text-base text-white">{text}</p>
       </div>
       </div>
       <a href={href} target="_blank" rel="noreferrer">
