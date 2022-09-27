62c4c96e2e07b504fbdd0a4d85231948322ee1c5
Afra Dawood <3frahmalik@gmail.com>
2022-09-27 11:12:18 +0400

    Border width changed

--- src/services/data-files/TimelineData.js
+++ src/services/data-files/TimelineData.js
@@ -6,7 +6,7 @@ const allTimelineData = [
         date: "20 Sep 2022 - 20 Oct 2022",
         image: "phase-1.png",
         status: true,
-        desktopBorder: "md:left-[100%] md:w-[130%] w-[370%] left-[100%] "
+        desktopBorder: "md:left-[100%] md:w-[130%] w-[150%] left-[100%] "
     },
     {
         phase: "PHASE 2",
@@ -24,7 +24,7 @@ const allTimelineData = [
         date: "29 Nov 2022 - 31 Dec 2022",
         image: "phase-3.png",
         status: false,
-        desktopBorder: "md:left-[100%]  md:w-[130%] w-[370%] left-[100%]"
+        desktopBorder: "md:left-[100%]  md:w-[130%] w-[150%] left-[100%]"
     },
     {
         phase: "PHASE 4",
