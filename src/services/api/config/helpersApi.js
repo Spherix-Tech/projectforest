a4f27c2f38423cb90e8dbd33b5d55c1b5209e024
Muzummil Iqbal <muzummil.iqbal@gmail.com>
2022-09-28 11:50:44 +0400

    New Header value

--- src/services/api/config/helpersApi.js
+++ src/services/api/config/helpersApi.js
@@ -2,4 +2,6 @@
 export const getHeadersAuthorization = (token) => ({
   Authorization: `Bearer ${token}`,
   "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers":
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
 });
